8a0fb04e6e050d1f8054d4f2db171d0eac8588b5
nikita9954 <kunitsa.nikita2@gmail.com>
2022-08-26 15:39:41 +0300

    метод 3 вопрос как сделать чтобы он сравнивал без учета регистра

--- src/DocumentNumber.java
+++ src/DocumentNumber.java
@@ -1,23 +1,49 @@
-
 public class DocumentNumber {
-    StringBuilder numberdocument=new StringBuilder();
-    void method(){
-        numberdocument.delete(4,10);
-        numberdocument.delete(8,18);
+    StringBuilder numberdocument = new StringBuilder();
+
+    void method() {
+        numberdocument.delete(4, 10);
+        numberdocument.delete(8, 18);
         System.out.println(numberdocument);
     }
-    void method1(){
-        numberdocument.replace(5,9,"****");
-        numberdocument.replace(15,19,"****");
+    void method1() {
+        numberdocument.replace(5, 9, "****");
+        numberdocument.replace(15, 19, "****");
         System.out.println(numberdocument);
     }
-    void method2(){
-        numberdocument.delete(0,5);
-        numberdocument.delete(4,10);
-        numberdocument.delete(8,13);
-        String c="Letters:";
-        String d=numberdocument.toString();
-        d.toUpperCase();
+    void method2() {
+        numberdocument.delete(0, 5);
+        numberdocument.delete(4, 10);
+        numberdocument.delete(8, 13);
+        String c = "Letters:";
+        String d = numberdocument.toString();
+        d = d.toUpperCase();
         System.out.println(c.concat(d));
+    }
+    void method4() {
+        String d = numberdocument.toString();
+        if (d.startsWith("5555")) {
+            System.out.println("Начинается с 1356");
+        } else System.out.println("Нет");
+    }
+    void method3() {
+        String d = numberdocument.toString();
+        if (d.contains("tynd")) {
+            System.out.println("Да cостоит");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+    void method5() {
+        String d = numberdocument.toString();
+        if (d.endsWith("1443")) {
+            System.out.println("Да заканчивается");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+    void method6() {
+        numberdocument.reverse();
+        System.out.println(numberdocument);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        DocumentNumber documentNumber=new DocumentNumber();
+        DocumentNumber documentNumber = new DocumentNumber();
         StringBuilder stringBuilder = documentNumber.numberdocument;
-        documentNumber.numberdocument= new StringBuilder("1356-tynd-4679-ryvb-1443");
+        documentNumber.numberdocument = new StringBuilder("1356-tynd-4679-ryvb-1443");
         //documentNumber.method();
         //documentNumber.method1();
-        documentNumber.method2();
+        //documentNumber.method2();
+        //documentNumber.method3(); как сделать чтобы он сравнивал без учета регистра
+        //documentNumber.method4();
+        //documentNumber.method5();
+        documentNumber.method6();
     }
 }
